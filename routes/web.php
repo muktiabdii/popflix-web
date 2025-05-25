b09a4c397da4cd9e729747e55c8aa61078d792b1
Gilang Hafizh <149552530+gilanghafizh@users.noreply.github.com>
2025-05-25 14:16:52 +0700

    Menambahkan Register

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MovieController::class, 'index'])->name('home');
 Route::get('/search', [MovieController::class, 'search'])->name('search');
 Route::get('/filter-genre', [MovieController::class, 'filterByGenre'])->name('filter.genre');
 Route::get('/movie/{id}', [MovieController::class, 'show'])->name('movie.show');
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
